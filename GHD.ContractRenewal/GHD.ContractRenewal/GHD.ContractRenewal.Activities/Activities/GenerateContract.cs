f92c16bb24b0e2321bbf717f8f69707c50561607
Jhon Carlo Concepcion <jhon.concepcion@ghd.com>
2022-05-30 18:04:40 +0800

    This commit include update in the replacement of items in the template out side optional and sow table

--- GHD.ContractRenewal/GHD.ContractRenewal/GHD.ContractRenewal.Activities/Activities/GenerateContract.cs
+++ GHD.ContractRenewal/GHD.ContractRenewal/GHD.ContractRenewal.Activities/Activities/GenerateContract.cs
@@ -3,9 +3,17 @@ using System.Activities;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Data;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
 using GHD.ContractRenewal.Activities.Properties;
 using UiPath.Shared.Activities;
 using UiPath.Shared.Activities.Localization;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace GHD.ContractRenewal.Activities
 {
@@ -98,16 +106,292 @@ namespace GHD.ContractRenewal.Activities
             var dataTableInfo = DataTableInfo.Get(context);
             var dataTableAnnual = DataTableAnnual.Get(context);
             var dataTableOptional = DataTableOptional.Get(context);
-    
-            ///////////////////////////
-            // Add execution logic HERE
-            ///////////////////////////
 
-            // Outputs
-            return (ctx) => {
-                ExceptionMessage.Set(ctx, null);
-                WordOutputPath.Set(ctx, null);
-            };
+            //Output
+            string strWordOutput = "";
+            string strExceptionMessage = "";
+            
+            
+
+
+            try
+            {
+                UpdateWordDoc(dataTableInfo, dataTableAnnual, dataTableOptional);
+            }
+            catch(Exception e)
+            {
+
+            }
+           
+            void UpdateWordDoc(DataTable dtInfo, DataTable dtAnnual, DataTable dtOptional)
+           {
+                Word.Application app = null;
+                Word.Documents docs = null;
+                Word.Document doc = null;
+                Word.Table tblCustomerDetails = null;
+                Word.Table tblOrderTerms = null;
+                Word.Table tblSOW = null;
+                Word.Table tblOptional = null;
+                Word.Range tblMethodologyResultsRng = null;
+                bool IsWordAppLaunched = false;
+                bool isBid = getDocType(dtInfo);
+                string strFileName = getFileName(dtInfo);
+                object missing = System.Reflection.Missing.Value;
+                Dictionary<string, string> dicInfo = buildCustomerInfo(dtInfo);
+
+
+                try
+                {
+                    Log("UpdateWordDoc" + " Method name:" + MethodBase.GetCurrentMethod().Name + " " + "UpdateWordDoc function start");
+                    app = new Word.Application();
+                    app.DisplayAlerts = Word.WdAlertLevel.wdAlertsNone;
+                    IsWordAppLaunched = true;
+                    docs = app.Documents;
+                    Log("UpdateWordDoc" + " Method name:" + MethodBase.GetCurrentMethod().Name + " " + "Word application object initialized");
+                    string newfolderName = DateTime.Now.ToString("MMddyyyy HHmmss");
+                    string newFilePath = CreateNewWordDoc(newfolderName, isBid, strFileName);
+                    doc = docs.Open(newFilePath, ReadOnly: false);
+                    Log("Opened word document");
+
+
+                    foreach (KeyValuePair<string, string> item in dicInfo)
+                    {
+                        /* This part loops through dicInfo and uses the key as the item to be searched in the word template.
+                         * 
+                         * app.Application.Selection.Find.Text = "[" + item.Key + "]"; this part searches to the word or item to be replaced.
+                         * The if and else part are the code that replaces the placeholder with the actual value.
+                         * The if part is for values with less than 256 in character length and the else part is for items equals or greater than 256;
+                         * 
+                         */
+                        object replaceAll = Word.WdReplace.wdReplaceAll;
+                        app.Application.Selection.Find.ClearFormatting();
+                        app.Application.Selection.Find.Text = "[" + item.Key + "]";
+                        app.Application.Selection.Find.Replacement.ClearFormatting();
+                        string replaceTxt = item.Value.Trim();
+                        if (replaceTxt.ToString().Length < 256)
+                        {
+                            app.Application.Selection.Find.Replacement.Text = replaceTxt;
+                            bool IsReplaced = app.Application.Selection.Find.Execute(
+                            ref missing, ref missing, ref missing, ref missing, ref missing,
+                            ref missing, ref missing, ref missing, ref missing, ref missing,
+                            ref replaceAll, ref missing, ref missing, ref missing, ref missing);
+                        }
+                        else
+                        {
+                            object findme = "[" + item.Key + "]";
+                            while (app.Application.Selection.Find.Execute(
+                                  ref findme, ref missing, ref missing, ref missing, ref missing,
+                                  ref missing, ref missing, ref missing, ref missing, ref missing,
+                                  ref missing, ref missing, ref missing, ref missing, ref missing))
+                            {
+
+                                app.Application.Selection.Text = replaceTxt;
+                                app.Application.Selection.Collapse();
+                            }
+                        }
+                    }
+
+
+                    doc.Save();
+                    doc.Close(ref missing, ref missing, ref missing);
+                    doc = null;
+                    Log("Document saved and closed successfully");
+
+                }
+                catch(Exception ex)
+                {
+                    strExceptionMessage = "Exception occured on generating the word document, this might be an issue with the supporting file, please check if the stockpile names are correct and please make sure that there is no merged cells." + "Error Code: " + ex.Message;
+                    Log("Exception Occured: " + ex + " occured in " + MethodBase.GetCurrentMethod().Name + "Error Code: " + ex.Message);
+
+
+
+                    if (IsWordAppLaunched)
+                    {
+                        app.Quit(SaveChanges: true, ref missing, ref missing);
+                        app = null;
+                    }
+                }
+                finally
+                {
+                    if (tblMethodologyResultsRng != null) { Marshal.ReleaseComObject(tblMethodologyResultsRng); }
+                    if (tblCustomerDetails != null) { Marshal.ReleaseComObject(tblCustomerDetails); }
+                    if (tblOrderTerms != null) { Marshal.ReleaseComObject(tblOrderTerms); }
+                    if (tblSOW != null) { Marshal.ReleaseComObject(tblSOW); }
+                    if (tblOptional != null) { Marshal.ReleaseComObject(tblOptional); }
+                    if (doc != null) { Marshal.ReleaseComObject(doc); }
+                    if (docs != null) { Marshal.ReleaseComObject(docs); }
+                    if (app != null) { Marshal.ReleaseComObject(app); }
+                }
+            }
+
+            Dictionary<string, string> buildCustomerInfo(DataTable dtInfo)
+            {
+                /*
+                 * Building dictionary of items to replace in the word doc template. 
+                 * This doesn't include replacing items in Optional and SOW tables.
+                 * Key should be the placeholder in the template. Note that the key is case sensitive.
+                 */
+                var customInfo = new Dictionary<string, string>();
+
+                foreach (DataRow drow in dtInfo.Rows)
+                {
+                    customInfo["Customer Name"] = drow["Customer Name"].ToString();
+                    customInfo["Primary Contact"] = drow["Primary Contact"].ToString();
+                    customInfo["Title"] = drow["Title"].ToString();
+                    customInfo["Email"] = drow["Email"].ToString();
+                    customInfo["Telephone"] = drow["Telephone"].ToString();
+                    customInfo["Address Street"] = drow["Address Street"].ToString();
+                    customInfo["Address City"] = drow["Address City, province"].ToString();
+                    customInfo["Address postal code"] = drow["Address postal code"].ToString();
+                    customInfo["Send Invoice"] = drow["Send Invoices to"].ToString();
+                    customInfo["Product Solution"] = drow["Product Solution"].ToString();
+                    customInfo["Project Number"] = drow["Project Number"].ToString();
+                    customInfo["Delivery Timing"] = drow["Estimated Delivery Timing"].ToString();
+                    customInfo["License Term"] = drow["License Term"].ToString();
+                    customInfo["Payment Terms"] = drow["Payment Terms"].ToString();
+                    customInfo["Expiry Date"] = drow["Quote Expiry Date"].ToString();
+                    customInfo["Executive"] = drow["Account Executive"].ToString();
+                    customInfo["Executive Email"] = drow["Account Executive Email"].ToString();
+                    customInfo["Executive Phone"] = drow["Account Executive Phone"].ToString();
+                    customInfo["Purpose"] = drow["Purpose"].ToString();
+                    customInfo["Total Implementation"] = drow["Total Implmentation Fee"].ToString();
+                    customInfo["Total Annual"] = drow["Total Annual Fee"].ToString();
+                }
+
+                return customInfo;
+
+            }
+
+            bool getDocType(DataTable dtInfo)
+            {
+                bool isBid = false;
+                try
+                {
+                    foreach (DataRow drow in dtInfo.Rows)
+                    {
+                        isBid = drow["Template to use"].ToString().Equals("bids&tenders");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log("Exception in GetDocType: " + e.Message);
+                }
+
+                return isBid;
+            }
+
+            string getFileName(DataTable dtInfo)
+            {
+                string FileName = "";
+                try
+                {
+                    foreach (DataRow drow in dtInfo.Rows)
+                    {
+                        FileName = drow["Name of File"].ToString();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log("Exception in GetDocType: " + ex.Message);
+                    strExceptionMessage = "Exception occured on generating the word document, this might be an issue with the supporting file, please check if the stockpile names are correct and please make sure that there is no merged cells." + "Error Code: " + ex.Message;
+                }
+
+                return FileName;
+            }
+
+
+            string CreateNewWordDoc(string newfolderName, bool IsBid, string fileName)
+            {
+                try
+                {
+                    //
+                    string BidTemplate = bidTemplatePath;
+                    string nonBidTemplate = nonBidTemplatePath;
+                    Log("Start: " + MethodBase.GetCurrentMethod().Name);
+
+                    //Create new file
+                    string newFileName = fileName + "_" + DateTime.Today.ToString("ddMMyyyy") + Path.GetExtension(BidTemplate);
+                    string newFilePath = Path.Combine(outputFolderPath, newfolderName, newFileName);
+                    string newFolderPath = Path.GetDirectoryName(newFilePath);
+                    strWordOutput = newFolderPath;
+
+
+                    Log("Create new file: " + newFileName);
+                    Log("New Word Path: " + strWordOutput);
+
+                    if (!Directory.Exists(newFolderPath))
+                    {
+                        Directory.CreateDirectory(newFolderPath);
+                        Log("Created new directory: " + newFolderPath);
+                    }
+                    if (IsBid)
+                    {
+                        File.Copy(BidTemplate, newFilePath);
+                    }
+                    else
+                    {
+                        File.Copy(nonBidTemplate, newFilePath);
+                    }
+
+
+                    Log("Copied template to new file path");
+                    Log("End: " + MethodBase.GetCurrentMethod().Name);
+                    
+                    return newFilePath;
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            void Log(object text)
+            {
+                bool isLogCreated = false;
+                StringBuilder sb = new StringBuilder();
+                try
+                {
+                    string logFileName = "Logs_UpdateWordDoc";
+                    string today = DateTime.Today.ToShortDateString().Replace("/", "_") + ".txt";
+                    if (!isLogCreated)
+                    {
+                        if (System.IO.Directory.Exists(logDirectory))
+                        {
+                            if (!File.Exists(System.IO.Path.Combine(logDirectory, logFileName + today)))
+                            {
+                                FileStream fs = File.Create(System.IO.Path.Combine(logDirectory, logFileName + today));
+                                fs.Close();
+                            }
+                        }
+                        else
+                        {
+                            System.IO.Directory.CreateDirectory(logDirectory);
+                            FileStream fs = File.Create(System.IO.Path.Combine(logDirectory, logFileName + today));
+                            fs.Close();
+                        }
+                        isLogCreated = true;
+                    }
+                    //Have not used else because the first text won't get appended when file gets created
+                    if (isLogCreated)
+                    {
+                        sb.Clear();
+                        sb.Append(DateTime.Now.ToString() + " " + text.ToString());
+                        File.AppendAllText(System.IO.Path.Combine(logDirectory, logFileName + today), "\r\n" + sb.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log("Exception occurred on Log Method:" + ex.ToString());
+                }
+            }
+
+              
+           // Outputs
+           return (ctx) => {
+                ExceptionMessage.Set(ctx, strExceptionMessage);
+                WordOutputPath.Set(ctx, strWordOutput);
+           };
         }
 
         #endregion
